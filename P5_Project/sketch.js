45c4cb5f7189b50fc861f75fb0bd93300e07c706
Xiaoyang Yu <xiyu0131@uni.sydney.edu.au>
2024-10-24 08:33:48 +1100

    LineGroupClass_testing
    I made a line group class, using 7 parameters, startX, startY, endX, endY, angle of line, number of lines, and color.
    The class has a draw function, using a forloop to control the size of the parallelogram. Also, the lines in each group are made parallel by giving a line spacing of 10. The distance between each starting points and between end points of the lines are calculated using the angle and spacing.
    The line groups stored into an array named as groups.
    In the main draw function, a for loop is used to draw each line group.

--- P5_Project/sketch.js
+++ P5_Project/sketch.js
@@ -1,7 +1,50 @@
+
+// Define a LineGroup class to manage the properties of each group of parallel lines
+class LineGroup {
+  constructor(startX, startY, endX, endY, angle, numLines, color) {
+    this.startX = startX;
+    this.startY = startY;
+    this.endX = endX;
+    this.endY = endY;
+    this.angle = angle;
+    this.numLines = numLines;
+    this.color = color;
+    this.lineSpacing = 10; // Spacing between lines
+  }
+
+  draw() {
+    stroke(this.color);
+    strokeWeight(1);
+
+    // Loop to create multiple lines in the group
+    for (let i = 0; i < this.numLines; i++) {
+      // Calculate new positions for each line based on the loop index
+      let offsetX = cos(this.angle) * this.lineSpacing * i;
+      let offsetY = sin(this.angle) * this.lineSpacing * i;
+
+      line(this.startX + offsetX, this.startY + offsetY, this.endX + offsetX, this.endY + offsetY);
+    }
+  }
+}
+
+let groups = [];
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
+  background(255);
+
+  // Create multiple LineGroup objects for different sections of the artwork
+  groups.push(new LineGroup(100, 100, 400, 100, radians(0), 20, color(0)));    // Black horizontal lines
+  groups.push(new LineGroup(200, 200, 500, 300, radians(30), 15, color(0)));   // Diagonal black lines
+  groups.push(new LineGroup(150, 500, 450, 550, radians(0), 20, color(150)));  // Gray horizontal lines
+
+  // Gray lines breaking the pattern
+  groups.push(new LineGroup(100, 300, 500, 350, radians(0), 5, color(150)));   // Gray parallel line section
 }
 
 function draw() {
-  background(220);
+  // Draw each group of lines
+  for (let group of groups) {
+    group.draw();
+  }
 }
