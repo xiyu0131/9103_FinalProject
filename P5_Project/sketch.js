bea292deefa50ed115613111c04d9070527cd9bf
Xiaoyang Yu <xiyu0131@uni.sydney.edu.au>
2024-10-24 17:19:17 +1100

    New Variable for the class
    Strokeweight and space are added into the constructor as variables

--- P5_Project/sketch.js
+++ P5_Project/sketch.js
@@ -1,7 +1,7 @@
 
 // Define a LineGroup class to manage the properties of each group of parallel lines
-class LineGroup {
-  constructor(startX, startY, endX, endY, angle, numLines, color) {
+class lineGroup {
+  constructor(startX, startY, endX, endY, angle, numLines, color, space, strokeWeight) {
     this.startX = startX;
     this.startY = startY;
     this.endX = endX;
@@ -9,12 +9,13 @@ class LineGroup {
     this.angle = angle;
     this.numLines = numLines;
     this.color = color;
-    this.lineSpacing = 10; // Spacing between lines
+    this.lineSpacing = space; // Spacing between lines
+    this.strokeWeight = strokeWeight;
   }
 
   draw() {
     stroke(this.color);
-    strokeWeight(1);
+    strokeWeight(this.strokeWeight);
 
     // Loop to create multiple lines in the group
     for (let i = 0; i < this.numLines; i++) {
@@ -34,12 +35,12 @@ function setup() {
   background(255);
 
   // Create multiple LineGroup objects for different sections of the artwork
-  groups.push(new LineGroup(100, 100, 400, 100, radians(0), 20, color(0)));    // Black horizontal lines
-  groups.push(new LineGroup(200, 200, 500, 300, radians(30), 15, color(0)));   // Diagonal black lines
-  groups.push(new LineGroup(150, 500, 450, 550, radians(0), 20, color(150)));  // Gray horizontal lines
+  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 20, color(0),0, 1));    // Black horizontal lines
+  groups.push(new lineGroup(200, 200, 500, 300, radians(30), 15, color(0),20,1));   // Diagonal black lines
+  groups.push(new lineGroup(150, 500, 450, 550, radians(0), 20, color(150),30, 5));  // Gray horizontal lines
 
   // Gray lines breaking the pattern
-  groups.push(new LineGroup(100, 300, 500, 350, radians(0), 5, color(150)));   // Gray parallel line section
+  groups.push(new lineGroup(100, 300, 500, 350, radians(0), 5, color(150),10, 10));   // Gray parallel line section
 }
 
 function draw() {
