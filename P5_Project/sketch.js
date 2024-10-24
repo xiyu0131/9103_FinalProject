526a5c85b93b3aafb8a2399042aadcd7a45ea1a5
Xiaoyang Yu <xiyu0131@uni.sydney.edu.au>
2024-10-24 17:53:53 +1100

    improving simplicity
    deleting some comments

--- P5_Project/sketch.js
+++ P5_Project/sketch.js
@@ -1,5 +1,5 @@
 
-// Define a LineGroup class to manage the properties of each group of parallel lines
+
 class lineGroup {
   constructor(startX, startY, endX, endY, angle, numLines, color, space, strokeWeight) {
     this.startX = startX;
@@ -9,7 +9,7 @@ class lineGroup {
     this.angle = angle;
     this.numLines = numLines;
     this.color = color;
-    this.lineSpacing = space; // Spacing between lines
+    this.lineSpacing = space; 
     this.strokeWeight = strokeWeight;
   }
 
@@ -17,9 +17,9 @@ class lineGroup {
     stroke(this.color);
     strokeWeight(this.strokeWeight);
 
-    // Loop to create multiple lines in the group
+ 
     for (let i = 0; i < this.numLines; i++) {
-      // Calculate new positions for each line based on the loop index
+      
       let offsetX = cos(this.angle) * this.lineSpacing * i;
       let offsetY = sin(this.angle) * this.lineSpacing * i;
 
@@ -34,17 +34,16 @@ function setup() {
   createCanvas(600, 600);
   background(255);
 
-  // Create multiple LineGroup objects for different sections of the artwork
-  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 20, color(0),0, 1));    // Black horizontal lines
-  groups.push(new lineGroup(200, 200, 500, 300, radians(30), 15, color(0),20,1));   // Diagonal black lines
-  groups.push(new lineGroup(150, 500, 450, 550, radians(0), 20, color(150),30, 5));  // Gray horizontal lines
+ 
+  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 20, color(0),0, 1));   
+  groups.push(new lineGroup(200, 200, 500, 300, radians(30), 15, color(0),20,1));   
+  groups.push(new lineGroup(150, 500, 450, 550, radians(0), 20, color(150),30, 5));  
 
-  // Gray lines breaking the pattern
-  groups.push(new lineGroup(100, 300, 500, 350, radians(0), 5, color(150),10, 10));   // Gray parallel line section
+ 
+  groups.push(new lineGroup(100, 300, 500, 350, radians(0), 5, color(150),10, 10));   
 }
 
 function draw() {
-  // Draw each group of lines
   for (let group of groups) {
     group.draw();
   }
