e7b8d35dc4a51a69b6ebf6c08e0e47b3a047b40c
Xiaoyang Yu <xiyu0131@uni.sydney.edu.au>
2024-10-25 17:50:11 +1100

    ResizeCanva function added & First layer is drawn
    The location of starting and ending points of the line groups are represented in the scale of window size.

--- P5_Project/sketch.js
+++ P5_Project/sketch.js
@@ -1,4 +1,6 @@
 
+
+
 //This class draws groups of lines using a class.
 //In this class, the startX, startY, endX, endY, angle, numLines, color, space, strokeWeight
 //are used as parameters.
@@ -31,17 +33,31 @@ class lineGroup {
 }
 
 //Storing the line groups into an array.
-let groups = [];
+//let groups = [];
+
+
 
 function setup() {
-  createCanvas(1000, 1000);
+  createCanvas(windowWidth, windowHeight);
   background(255);
+  /*let baseLayerStartX=0.3*width;
+  let baseLayerStartY=0.8*height;
+  let baseLayerEndX=0.7*width;
+  let baseLayerEndY=0.2*height;
+  let baseLayerAngle=-radians(PI/3);
+
+
+  groups.push(new lineGroup(baseLayerStartX, baseLayerStartY, baseLayerEndX, baseLayerEndY,baseLayerAngle, random(20), random(100,200),20,3));
+
+
 
   groups.push(new lineGroup(600, 50, 100, 400, radians(80), 13, color(200,200,0),1, 5));  
-  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 1, color(0),100, 5));   
-  groups.push(new lineGroup(200, 200, 500, 300, -radians(30), 15, color(0),20,1));   
-  groups.push(new lineGroup(150, 500, 450, 200, -radians(0), 20, color(150),10, 3));  
+  */
+  //groups.push(new lineGroup(100, 100, 400, 100, radians(0), 1, color(0),100, 5));   
+  //groups.push(new lineGroup(200, 200, 500, 300, -radians(30), 15, color(0),20,1));   
+  //groups.push(new lineGroup(150, 500, 450, 200, -radians(0), 20, color(150),10, 3));  
 
+  drawLineGroups();
   
 }
 
@@ -50,4 +66,40 @@ function draw() {
   for (let group of groups) {
     group.draw();
   }
+  
+}
+
+function drawLineGroups() {
+  // Clear existing groups when resizing
+  groups = [];
+  
+  let baseLayer1StartX = 0.15 * windowWidth;
+  let baseLayer1StartY = 0.75 * windowHeight;
+  let baseLayer1EndX = 0.544 * windowWidth;
+  let baseLayer1EndY = 0.4 * windowHeight;
+  let baseLayer1Angle = PI / 6.78;
+
+  let baseLayer2StartX = 0.582 * windowWidth;
+  let baseLayer2StartY = 0.325 * windowHeight;
+  let baseLayer2EndX = 0.72 * windowWidth;
+  let baseLayer2EndY = 0.203 * windowHeight;
+  let baseLayer2Angle = PI / 6.78; 
+
+  let baseLayer3StartX = 0.2314 * windowWidth;
+  let baseLayer3StartY = 0.814 * windowHeight;
+  let baseLayer3EndX = 0.83 * windowWidth;
+  let baseLayer3EndY = 0.285 * windowHeight;
+  let baseLayer3Angle = PI / 6.78; 
+
+
+  groups.push(new lineGroup(baseLayer1StartX, baseLayer1StartY, baseLayer1EndX, baseLayer1EndY, baseLayer1Angle, 3, color(100,0,200), 10, 3));
+  groups.push(new lineGroup(baseLayer2StartX, baseLayer2StartY, baseLayer2EndX, baseLayer2EndY, baseLayer2Angle, 3, color(100,0,200), 10, 3));
+  groups.push(new lineGroup(baseLayer3StartX, baseLayer3StartY, baseLayer3EndX, baseLayer3EndY, baseLayer3Angle, 6, color(100,0,200), 5, 3));
+  //groups.push(new lineGroup(600, 50, 100, 400, radians(80), 13, color(200, 200, 0), 1, 5));
+}
+
+function windowResized() {
+  // Adjust canvas size and redraw the shape when the window is resized
+  resizeCanvas(windowWidth, windowHeight);
+  drawLineGroups();
 }
