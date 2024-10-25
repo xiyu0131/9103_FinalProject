88ef552218d2854f7952c83d8d032faa4356f48a
Xiaoyang Yu <xiyu0131@uni.sydney.edu.au>
2024-10-25 12:31:55 +1100

    Changing the shapes of line groups
    Several line groups are drawn, following the shape of the chosen artowrk.

--- P5_Project/sketch.js
+++ P5_Project/sketch.js
@@ -1,5 +1,7 @@
 
-
+//This class draws groups of lines using a class.
+//In this class, the startX, startY, endX, endY, angle, numLines, color, space, strokeWeight
+//are used as parameters.
 class lineGroup {
   constructor(startX, startY, endX, endY, angle, numLines, color, space, strokeWeight) {
     this.startX = startX;
@@ -28,20 +30,21 @@ class lineGroup {
   }
 }
 
+//Storing the line groups into an array.
 let groups = [];
 
 function setup() {
-  createCanvas(600, 600);
+  createCanvas(1000, 1000);
   background(255);
 
- 
-  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 20, color(0),0, 1));   
-  groups.push(new lineGroup(200, 200, 500, 300, radians(30), 15, color(0),20,1));   
-  groups.push(new lineGroup(150, 500, 450, 550, radians(0), 20, color(150),30, 5));  
+  groups.push(new lineGroup(600, 50, 100, 400, radians(80), 13, color(200,200,0),1, 5));  
+  groups.push(new lineGroup(100, 100, 400, 100, radians(0), 1, color(0),100, 5));   
+  groups.push(new lineGroup(200, 200, 500, 300, -radians(30), 15, color(0),20,1));   
+  groups.push(new lineGroup(150, 500, 450, 200, -radians(0), 20, color(150),10, 3));  
 
- 
-  groups.push(new lineGroup(100, 300, 500, 350, radians(0), 5, color(150),10, 10));   
+  
 }
+
 
 function draw() {
   for (let group of groups) {
